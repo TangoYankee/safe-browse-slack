bdd8fed7831c9bef1e4ca4302fec399c7eadd71e
Timothy Miller <miller.tim108@gmail.com>
2019-07-15 17:22:51 -0700

    Header (#9)
    * readme typo
    
    * transer to pug
    
    * remove main subfolder
    
    * favicon and css setup
    
    * insert demo gif, style footer
    
    * install jquery. responsively pad body above footer
    
    * add to slack button
    
    * responsive text header
    
    * add image, style header
    
    * remove unused svg icon

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+
+function footerAlign(){
+    $('footer').css('height', 'auto');
+    var footerHeight = $('footer').outerHeight();
+    $('body').css('padding-bottom', footerHeight);
+    $('footer').css('height', footerHeight);
+}
+
+function headerAlign(){
+    $('header').css('height', 'auto');
+    var headerHeight = $('header').outerHeight();
+    $('body').css('padding-top', headerHeight);
+    $('header').css('height', footerHeight);
+}
+
+$(document).ready(()=>{
+    footerAlign();
+    headerAlign();
+})
+
+$(window).resize(()=>{
+    footerAlign();
+    headerAlign();
+})
