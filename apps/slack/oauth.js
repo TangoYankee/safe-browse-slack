5c949035c773bf052020bf4980f9cd8dae51c9ec
Timothy Miller <miller.tim108@gmail.com>
2019-07-31 15:00:00 -0700

    Revert "Oauth (#21)" (#22)
    This reverts commit e3881a5de67df46e2dc1731ebacf2d8e4cd84d7a.

--- apps/slack/oauth.js
+++ apps/slack/oauth.js
@@ -1,12 +1,8 @@
-const config = require('./config.js');
-const request = require('request');
-const crypto = require('crypto');
-const cryptoRandomString = require('crypto-random-string');
-const iv_len = 16;
-
+var config = require('./config.js');
+var request = require('request');
 
 oauth = (req, res) => {
-    /*compose Slack oauth token request*/
+    /*compose Slack credentials*/
     if (!req.query.code) {
         res.status(500);
         res.send({ "Error": "Code not received." });
@@ -28,35 +24,10 @@ getOauth = (res, url, query_string) => {
             res.send(error.toString());
         } else {
             res.json(body);
-            // Send the authorization token to be encrypted
-            // Add the encrypted token to the database
+
         }
     })
 }
 
 
-encryptToken = (token_plain, token_key) => {
-    /*Encrypt token to store at rest*/
-    let algorithm = "aes-256-cbc";
-    let iv = cryptoRandomString({length: iv_len, type: 'hex'});
-    let cipher = crypto.createCipheriv(algorithm, token_key, iv);
-    let encrypted = cipher.update(token_plain, 'utf8', 'hex');
-    encrypted += cipher.final('hex');
-    return `${encrypted}${iv}`;
-}
-
-
-decryptToken = (token_cipher, token_key) => {
-    /* Decrypt token to send for authorization */
-    let algorithm = "aes-256-cbc";
-    let encrypted = token_cipher.slice(0, 160);
-    let iv = token_cipher.slice(160);
-    let decipher = crypto.createDecipheriv(algorithm, token_key, iv);
-    let decrypted = decipher.update(encrypted, "hex", "utf8");
-    decrypted += decipher.final("utf8");
-    return decrypted;
-}
-
-
-// export encrypt, decrypt
-module.exports = {oauth, encryptToken, decryptToken};
+module.exports = oauth;
