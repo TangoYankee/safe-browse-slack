e3881a5de67df46e2dc1731ebacf2d8e4cd84d7a
Timothy Miller <miller.tim108@gmail.com>
2019-07-30 20:26:59 -0700

    Oauth (#21)
    * readme typo
    
    * transer to pug
    
    * remove main subfolder
    
    * favicon and css setup
    
    * insert demo gif, style footer
    
    * install jquery. responsively pad body above footer
    
    * add to slack button
    
    * responsive text header
    
    * add image, style header
    
    * remove unused svg icon
    
    * optimize demo gif
    
    * finish first draft of home page
    
    * object fit, max-width
    
    * text-align center, remove border for image
    
    * overview and preview
    
    * feedback, installation
    
    * local setup and testing
    
    * describe tests
    
    * describe format functions
    
    * describe main, slack, and methods functions
    
    * remove display text and link address tests
    
    * move data to just above tests
    
    * check all character positions are numbers
    
    * additional test messages, check for blanks and url spaces
    
    * outline tests of valid link strings and urls
    
    * operating unit tests for valid strings and urls
    
    * refactor character position arrays
    
    * add token placeholders to config example
    
    * outline signing signature module
    
    * incorporate signature check into main flow
    
    * outline tests for signature module
    
    * implement tests
    
    * dead end. body is encoded at the generic level. need to encode at route level
    
    * install qs
    
    * format with rfc1738
    
    * debug parameters and case of calls
    
    * format current time
    
    * check equality with timingSafeEqual
    
    * hide test data
    
    * install mongoose
    
    * outline encryption, install crypto
    
    * generate fake keys and tokens
    
    * code outline of encryption tests
    
    * shotty attempt at encryption implemented
    
    * change cipher to token in config file

---
+++ apps/database/db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+var OAuthSchema = new mongoose.Schema({
+    // team_id (string)
+    // user_id (string)
+    // token (string)
+    // scope (array of strings)
+})
--- apps/slack/oauth.js
+++ apps/slack/oauth.js
@@ -1,8 +1,12 @@
-var config = require('./config.js');
-var request = require('request');
+const config = require('./config.js');
+const request = require('request');
+const crypto = require('crypto');
+const cryptoRandomString = require('crypto-random-string');
+const iv_len = 16;
+
 
 oauth = (req, res) => {
-    /*compose Slack credentials*/
+    /*compose Slack oauth token request*/
     if (!req.query.code) {
         res.status(500);
         res.send({ "Error": "Code not received." });
@@ -24,10 +28,35 @@ getOauth = (res, url, query_string) => {
             res.send(error.toString());
         } else {
             res.json(body);
-
+            // Send the authorization token to be encrypted
+            // Add the encrypted token to the database
         }
     })
 }
 
 
-module.exports = oauth;
+encryptToken = (token_plain, token_key) => {
+    /*Encrypt token to store at rest*/
+    let algorithm = "aes-256-cbc";
+    let iv = cryptoRandomString({length: iv_len, type: 'hex'});
+    let cipher = crypto.createCipheriv(algorithm, token_key, iv);
+    let encrypted = cipher.update(token_plain, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+    return `${encrypted}${iv}`;
+}
+
+
+decryptToken = (token_cipher, token_key) => {
+    /* Decrypt token to send for authorization */
+    let algorithm = "aes-256-cbc";
+    let encrypted = token_cipher.slice(0, 160);
+    let iv = token_cipher.slice(160);
+    let decipher = crypto.createDecipheriv(algorithm, token_key, iv);
+    let decrypted = decipher.update(encrypted, "hex", "utf8");
+    decrypted += decipher.final("utf8");
+    return decrypted;
+}
+
+
+// export encrypt, decrypt
+module.exports = {oauth, encryptToken, decryptToken};
