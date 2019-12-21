e6432c1cf35b62d92140a9ef313863162eb5069c
tangoyankee <miller.tim108@gmail.com>
2019-12-20 19:09:24 -0800

    lint

--- apps/safe-browse/test-data/request-body-data.js
+++ apps/safe-browse/test-data/request-body-data.js
@@ -1,50 +1,50 @@
 const requestBodyOne = {
-    client: {
-      clientId: 'markdownlinks',
-      clientVersion: '1.5.2'
-    },
-    threatInfo: {
-      platformTypes: ['ANY_PLATFORM'],
-      threatEntries: [
-        { url: 'testsafebrowsing.appspot.com/s/phishing.html' },
-        { url: 'testsafebrowsing.appspot.com/s/unwanted.html' },
-        { url: 'testsafebrowsing.appspot.com/s/malware.html' },
-        { url: 'nasa.gov' }
-      ],
-      threatEntryTypes: ['URL'],
-      threatTypes: [
-        'THREAT_TYPE_UNSPECIFIED',
-        'MALWARE',
-        'SOCIAL_ENGINEERING',
-        'UNWANTED_SOFTWARE',
-        'POTENTIALLY_HARMFUL_APPLICATION'
-      ]
-    }
+  client: {
+    clientId: 'markdownlinks',
+    clientVersion: '1.5.2'
+  },
+  threatInfo: {
+    platformTypes: ['ANY_PLATFORM'],
+    threatEntries: [
+      { url: 'testsafebrowsing.appspot.com/s/phishing.html' },
+      { url: 'testsafebrowsing.appspot.com/s/unwanted.html' },
+      { url: 'testsafebrowsing.appspot.com/s/malware.html' },
+      { url: 'nasa.gov' }
+    ],
+    threatEntryTypes: ['URL'],
+    threatTypes: [
+      'THREAT_TYPE_UNSPECIFIED',
+      'MALWARE',
+      'SOCIAL_ENGINEERING',
+      'UNWANTED_SOFTWARE',
+      'POTENTIALLY_HARMFUL_APPLICATION'
+    ]
   }
+}
 
-  const requestBodyTwo = {
-    client: {
-      clientId: 'markdownlinks',
-      clientVersion: '1.5.2'
-    },
-    threatInfo: {
-      platformTypes: ['ANY_PLATFORM'],
-      threatEntries: [
-        { url: 'testsafebrowsing.appspot.com/s/unwanted.html' },
-        { url: 'testsafebrowsing.appspot.com/s/malware.html' },
-        { url: 'nasa.gov' }
-      ],
-      threatEntryTypes: ['URL'],
-      threatTypes: [
-        'THREAT_TYPE_UNSPECIFIED',
-        'MALWARE',
-        'SOCIAL_ENGINEERING',
-        'UNWANTED_SOFTWARE',
-        'POTENTIALLY_HARMFUL_APPLICATION'
-      ]
-    }
+const requestBodyTwo = {
+  client: {
+    clientId: 'markdownlinks',
+    clientVersion: '1.5.2'
+  },
+  threatInfo: {
+    platformTypes: ['ANY_PLATFORM'],
+    threatEntries: [
+      { url: 'testsafebrowsing.appspot.com/s/unwanted.html' },
+      { url: 'testsafebrowsing.appspot.com/s/malware.html' },
+      { url: 'nasa.gov' }
+    ],
+    threatEntryTypes: ['URL'],
+    threatTypes: [
+      'THREAT_TYPE_UNSPECIFIED',
+      'MALWARE',
+      'SOCIAL_ENGINEERING',
+      'UNWANTED_SOFTWARE',
+      'POTENTIALLY_HARMFUL_APPLICATION'
+    ]
   }
-  
+}
+
 module.exports = {
   requestBodyOne,
   requestBodyTwo
--- apps/safe-browse/test-data/threat-matches-data.js
+++ apps/safe-browse/test-data/threat-matches-data.js
@@ -1,59 +1,59 @@
 const threatMatchOne = {
-    matches: [
-      {
-        threatType: 'SOCIAL_ENGINEERING',
-        platformType: 'ANY_PLATFORM',
-        threat: {
-          url: 'testsafebrowsing.appspot.com/s/phishing.html'
-        },
-        cacheDuration: '300s',
-        threatEntryType: 'URL'
+  matches: [
+    {
+      threatType: 'SOCIAL_ENGINEERING',
+      platformType: 'ANY_PLATFORM',
+      threat: {
+        url: 'testsafebrowsing.appspot.com/s/phishing.html'
       },
-      {
-        threatType: 'UNWANTED_SOFTWARE',
-        platformType: 'ANY_PLATFORM',
-        threat: {
-          url: 'testsafebrowsing.appspot.com/s/unwanted.html'
-        },
-        cacheDuration: '300s',
-        threatEntryType: 'URL'
+      cacheDuration: '300s',
+      threatEntryType: 'URL'
+    },
+    {
+      threatType: 'UNWANTED_SOFTWARE',
+      platformType: 'ANY_PLATFORM',
+      threat: {
+        url: 'testsafebrowsing.appspot.com/s/unwanted.html'
       },
-      {
-        threatType: 'MALWARE',
-        platformType: 'ANY_PLATFORM',
-        threat: {
-          url: 'testsafebrowsing.appspot.com/s/malware.html'
-        },
-        cacheDuration: '300s',
-        threatEntryType: 'URL'
-      }
-    ]
-  }
+      cacheDuration: '300s',
+      threatEntryType: 'URL'
+    },
+    {
+      threatType: 'MALWARE',
+      platformType: 'ANY_PLATFORM',
+      threat: {
+        url: 'testsafebrowsing.appspot.com/s/malware.html'
+      },
+      cacheDuration: '300s',
+      threatEntryType: 'URL'
+    }
+  ]
+}
 
-  const threatMatchTwo = {
-    matches: [
-      {
-        threatType: 'UNWANTED_SOFTWARE',
-        platformType: 'ANY_PLATFORM',
-        threat: {
-          url: 'testsafebrowsing.appspot.com/s/unwanted.html'
-        },
-        cacheDuration: '300s',
-        threatEntryType: 'URL'
+const threatMatchTwo = {
+  matches: [
+    {
+      threatType: 'UNWANTED_SOFTWARE',
+      platformType: 'ANY_PLATFORM',
+      threat: {
+        url: 'testsafebrowsing.appspot.com/s/unwanted.html'
       },
-      {
-        threatType: 'MALWARE',
-        platformType: 'ANY_PLATFORM',
-        threat: {
-          url: 'testsafebrowsing.appspot.com/s/malware.html'
-        },
-        cacheDuration: '300s',
-        threatEntryType: 'URL'
-      }
-    ]
-  }
+      cacheDuration: '300s',
+      threatEntryType: 'URL'
+    },
+    {
+      threatType: 'MALWARE',
+      platformType: 'ANY_PLATFORM',
+      threat: {
+        url: 'testsafebrowsing.appspot.com/s/malware.html'
+      },
+      cacheDuration: '300s',
+      threatEntryType: 'URL'
+    }
+  ]
+}
 
 module.exports = {
-    threatMatchOne,
-    threatMatchTwo
+  threatMatchOne,
+  threatMatchTwo
 }
