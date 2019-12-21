7d4101054cc828b7b40c13f6093e485307d927cd
tangoyankee <miller.tim108@gmail.com>
2019-12-21 08:26:35 -0800

    tests for hypertext

--- apps/hyper-text/test-data/set-message-data/input-text-data.js
+++ apps/hyper-text/test-data/set-message-data/input-text-data.js
@@ -1,5 +1,5 @@
 inputTextOne = '[Phishing Site](testsafebrowsing.appspot.com/s/phishing.html), [Unwanted Software](https://testsafebrowsing.appspot.com/s/unwanted.html), [Malware Site](testsafebrowsing.appspot.com/s/malware.html), and [Nasa](nasa.gov)'
 
 module.exports = {
-    inputTextOne
+  inputTextOne
 }
