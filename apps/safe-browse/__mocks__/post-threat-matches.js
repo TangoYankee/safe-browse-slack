123ffe93b7bf25f65aba9d03cfcfbbfb45ef3e9d
tangoyankee <miller.tim108@gmail.com>
2019-12-25 11:02:15 -0800

    rewrite mock postthreatmatches to reflect true api response

--- apps/safe-browse/__mocks__/post-threat-matches.js
+++ apps/safe-browse/__mocks__/post-threat-matches.js
@@ -5,13 +5,16 @@ const postThreatMatches = (requestBody) => {
   var customResponse = response()
   return new Promise(resolve => {
     if (requestBody.threatInfo) {
+      var matches = []
       for (var threatEntry of requestBody.threatInfo.threatEntries) {
         var match = suspectedThreats.find(matches => matches.threat.url === threatEntry.url)
         if (match) {
-          customResponse.body['matches'] = []
-          customResponse.body.matches.push(match)
+          matches.push(match)
         }
       }
+    }
+    if (matches.length >= 1) {
+      customResponse.body['matches'] = matches
     }
     console.log(JSON.stringify(customResponse.body))
     resolve(customResponse.body)
