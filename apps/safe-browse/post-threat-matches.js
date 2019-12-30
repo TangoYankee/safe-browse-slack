5a2b1d74d9efbd668b1d5792c26a280a5750d092
Timothy Miller <miller.tim108@gmail.com>
2019-12-30 09:23:27 -0800

    add timeout to safebrowse request (#148)

--- apps/safe-browse/post-threat-matches.js
+++ apps/safe-browse/post-threat-matches.js
@@ -4,10 +4,12 @@ const request = require('request')
 const postThreatMatches = (requestBody) => {
   /* find threats that safe browse suspects */
   var requestUrl = 'https://safebrowsing.googleapis.com/v4/threatMatches:find'
+  // Add timeout of 30s as an option
   var options = {
     url: requestUrl,
     body: requestBody,
     json: true,
+    timeout: 1500,
     qs: { key: process.env.GOOGLE_SAFE_BROWSING_KEY }
   }
   return new Promise(resolve => {
