f204c0d38d3924118eb5c7e45623cdb446f44f2d
tangoyankee <miller.tim108@gmail.com>
2020-02-16 12:22:26 -0800

    lint spacing

--- apps/credential/__tests__/signature-test.js
+++ apps/credential/__tests__/signature-test.js
@@ -3,101 +3,98 @@
 const { Signature } = require('../signature')
 const { slackRequest } = require('../test-data/signature-data')
 
-
 describe('the request is invalid, with a recent time but a wrong signature', () => {
   var signature
-  beforeAll(() =>{
+  beforeAll(() => {
     signature = new Signature(slackRequest)
-    signature.timestamp = Math.floor(new Date().getTime()/ 1e3)
-    signature.currentTime = Math.floor(new Date().getTime()/ 1e3) + 1e2
+    signature.timestamp = Math.floor(new Date().getTime() / 1e3)
+    signature.currentTime = Math.floor(new Date().getTime() / 1e3) + 1e2
   })
 
   it('should be a recent call',
-  () => {
-    expect(signature._isRecent).toBe(true)
-  })
+    () => {
+      expect(signature._isRecent).toBe(true)
+    })
 
   it('should have an incorrect signature',
-  () => {
-    expect(signature._isCorrectSignature).toBe(false)
-  })
+    () => {
+      expect(signature._isCorrectSignature).toBe(false)
+    })
 
   it('should be an invalid request',
-  () => {
-    expect(signature.isValid).toBe(false)
-  })
-
-
+    () => {
+      expect(signature.isValid).toBe(false)
+    })
 })
 
 describe('the request is invalid, with a correct signature but an old time', () => {
   var signature
-  beforeAll(() =>{
+  beforeAll(() => {
     signature = new Signature(slackRequest)
     signature.timestamp = '1531420618'
     signature.currentTime = '2e10'
   })
 
   it('should be an old call',
-  () => {
-    expect(signature._isRecent).toBe(false)
-  })
+    () => {
+      expect(signature._isRecent).toBe(false)
+    })
 
   it('should be a correct signature',
-  () => {
-    expect(signature._isCorrectSignature).toBe(true)
-  })
+    () => {
+      expect(signature._isCorrectSignature).toBe(true)
+    })
 
   it('should be an invalid request',
-  () => {
-    expect(signature.isValid).toBe(false)
-  })
+    () => {
+      expect(signature.isValid).toBe(false)
+    })
 })
 
 describe('the request is invalid, with a wrong signature and an old time', () => {
   var signature
-  beforeAll(() =>{
+  beforeAll(() => {
     signature = new Signature(slackRequest)
     signature.timestamp = '0'
     signature.currentTime = '4e2'
   })
 
   it('should be an old call',
-  () => {
-    expect(signature._isRecent).toBe(false)
-  })
+    () => {
+      expect(signature._isRecent).toBe(false)
+    })
 
   it('should be a wrong signature',
-  () => {
-    expect(signature._isCorrectSignature).toBe(false)
-  })
+    () => {
+      expect(signature._isCorrectSignature).toBe(false)
+    })
 
   it('should be an invalid request',
-  () => {
-    expect(signature.isValid).toBe(false)
-  })
+    () => {
+      expect(signature.isValid).toBe(false)
+    })
 })
 
 describe('the request is valid, with a correct signature and a recent time', () => {
   var signature
-  beforeAll(() =>{
+  beforeAll(() => {
     signature = new Signature(slackRequest)
     signature.timestamp = '1531420618'
     signature.currentTime = '1531420618'
   })
 
   it('should be a recent call',
-  () => {
-    expect(signature._isRecent).toBe(true)
-  })
+    () => {
+      expect(signature._isRecent).toBe(true)
+    })
 
   it('should be a correct signature',
-  () => {
-    expect(signature._isCorrectSignature).toBe(true)
-  })
+    () => {
+      expect(signature._isCorrectSignature).toBe(true)
+    })
 
   it('should be an valid request',
-  () => {
-    expect(signature.isValid).toBe(true)
-  })
+    () => {
+      expect(signature.isValid).toBe(true)
+    })
 })
--- apps/credential/signature.js
+++ apps/credential/signature.js
@@ -5,12 +5,12 @@ const qs = require('qs')
 
 class Signature {
   /* request is properly signed */
-  constructor(req){
+  constructor (req) {
     this.req = req
     this.version = 'v0'
     this.timeTolerance = 3e2
     this.timestamp = Number(req.headers['x-slack-request-timestamp'])
-    this.currentTime = Math.floor(new Date().getTime()/ 1e3)
+    this.currentTime = Math.floor(new Date().getTime() / 1e3)
   }
 
   get isValid () {
@@ -20,23 +20,23 @@ class Signature {
 
   get _isRecent () {
     /* guard against replay attacks */
-    var timeDelta = Math.abs(this.currentTime-this.timestamp)
+    var timeDelta = Math.abs(this.currentTime - this.timestamp)
     return (timeDelta <= this.timeTolerance)
   }
 
   get _isCorrectSignature () {
     /* calculated application signature and slack signature match */
-      var reqBody = qs.stringify(this.req.body, { format: 'RFC1738' })
-      var baseString = `${this.version}:${this.timestamp}:${reqBody}`
-      var signingSecret = process.env.SLACK_SIGNING_SECRET
-      var hexDigest = crypto.createHmac('sha256', signingSecret).update(`${baseString}`).digest('hex')
-      var appSignature = `${this.version}=${hexDigest}`
-      var slackSignature = this.req.headers['x-slack-signature']
-      return (crypto.timingSafeEqual(
-        Buffer.from(appSignature, 'utf-8'),
-        Buffer.from(slackSignature, 'utf-8'))
-      )
-    }
+    var reqBody = qs.stringify(this.req.body, { format: 'RFC1738' })
+    var baseString = `${this.version}:${this.timestamp}:${reqBody}`
+    var signingSecret = process.env.SLACK_SIGNING_SECRET
+    var hexDigest = crypto.createHmac('sha256', signingSecret).update(`${baseString}`).digest('hex')
+    var appSignature = `${this.version}=${hexDigest}`
+    var slackSignature = this.req.headers['x-slack-signature']
+    return (crypto.timingSafeEqual(
+      Buffer.from(appSignature, 'utf-8'),
+      Buffer.from(slackSignature, 'utf-8'))
+    )
+  }
 }
 
 module.exports = {
