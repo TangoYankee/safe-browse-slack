0868fbc7b501511a99577f437dc497251d3b4c4a
tangoyankee <miller.tim108@gmail.com>
2020-02-16 21:58:45 -0800

    integrate token crypto object into OAuth

--- apps/credential/oauth.js
+++ apps/credential/oauth.js
@@ -1,11 +1,10 @@
 'use strict'
 
-const crypto = require('crypto')
-const cryptoRandomString = require('crypto-random-string')
 const process = require('process')
 const queryString = require('querystring')
 const request = require('request')
 const { saveTeam } = require('../database/db.js')
+const { TokenCrypto } = require('./token-crypto')
 
 const oauth = (req, res) => {
   /* compose Slack credentials */
@@ -41,7 +40,7 @@ const postOAuth = (res, url, thisQueryString) => {
       var teamId = bodyJson.team.id
       var accessTokenPlain = bodyJson.access_token
       if (teamId && accessTokenPlain) {
-        var accessTokenCipher = encryptToken(accessTokenPlain, process.env.SLACK_OAUTH_TOKEN_SECRET)
+        var accessTokenCipher = new TokenCrypto().encrypt(accessTokenPlain)
         saveTeam(teamId, accessTokenCipher)
         thisQueryMessage = queryString.stringify({ message: 'success' })
         res.redirect('/?' + thisQueryMessage)
@@ -54,25 +53,4 @@ const postOAuth = (res, url, thisQueryString) => {
   })
 }
 
-const algorithm = 'aes-256-cbc'
-const encryptToken = (tokenPlain, tokenKey) => {
-  /* encrypt token to store at rest */
-  var ivLen = 16
-  var iv = cryptoRandomString({ length: ivLen, type: 'hex' })
-  var cipher = crypto.createCipheriv(algorithm, tokenKey, iv)
-  let encrypted = cipher.update(tokenPlain, 'utf8', 'hex')
-  encrypted += cipher.final('hex')
-  return `${encrypted}${iv}`
-}
-
-const decryptToken = (tokenCipher, tokenKey) => {
-  /* decrypt token to send for authorization */
-  var encrypted = tokenCipher.slice(0, 160)
-  var iv = tokenCipher.slice(160)
-  var decipher = crypto.createDecipheriv(algorithm, tokenKey, iv)
-  let decrypted = decipher.update(encrypted, 'hex', 'utf8')
-  decrypted += decipher.final('utf8')
-  return decrypted
-}
-
-module.exports = { oauth, encryptToken, decryptToken }
+module.exports = { oauth }
