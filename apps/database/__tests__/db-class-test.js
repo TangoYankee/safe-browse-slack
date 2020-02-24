3b22bab6ab3701c5affbd47c236f63e6904eeee6
tangoyankee <miller.tim108@gmail.com>
2020-02-23 16:01:32 -0800

    write tests for db class

--- apps/database/__tests__/db-class-test.js
+++ apps/database/__tests__/db-class-test.js
@@ -1,18 +1,19 @@
 'use strict'
 
 const MongoClient = require('mongodb').MongoClient
-const { checkTeam } = require('../db')
+const { Database } = require('../db-class')
 const { teamId, teamTokenOne, teamTokentwo } = require('../test-data/db-data')
 
-describe('insert', () => {
+describe('insert and update a team token', () => {
   var connection
   var db
-
+  var markdownlinksdb
+  var teams
   beforeAll(async () => {
-    connection = await MongoClient.connect(global.__MONGO_URI__, {
-      useNewUrlParser: true
-    })
+    connection = await MongoClient.connect(global.__MONGO_URI__, {useNewUrlParser: true})
     db = connection.db(global.__MONGO_DB_NAME__)
+    teams = db.collection('teams')
+    markdownlinksdb = new Database(db)
   })
 
   afterAll(async () => {
@@ -20,22 +21,16 @@ describe('insert', () => {
     await db.close()
   })
 
-  it(
-    'insert a team into the collection',
+  it('inserts a team into the collection',
     async () => {
-      var teams = db.collection('teams')
-      await checkTeam(teamId, teamTokenOne, teams)
+      await markdownlinksdb.storeTeamToken(teamId, teamTokenOne)
       var insertedTeam = await teams.findOne({ team_id: teamId })
       expect(teamTokenOne).toEqual(insertedTeam.access_token_cipher)
     })
 
-  it(
-    'update a team with a new token',
+  it('updates the team with a new token',
     async () => {
-      var teams = db.collection('teams')
-      var insertedTeam = await teams.findOne({ team_id: teamId })
-      expect(teamTokenOne).toEqual(insertedTeam.access_token_cipher)
-      await checkTeam(teamId, teamTokentwo, teams)
+      await markdownlinksdb.storeTeamToken(teamId, teamTokentwo)
       var updatedTeam = await teams.findOne({ team_id: teamId })
       expect(teamTokentwo).toEqual(updatedTeam.access_token_cipher)
     })
--- apps/database/db-class.js
+++ apps/database/db-class.js
@@ -5,44 +5,34 @@ const MongoClient = require('mongodb').MongoClient
 
 // var clusterUri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ty-db-xadwv.mongodb.net/markdownlinksdb?retryWrites=true&w=majority`
 
-class Database {
-  constructor (databaseName) {
-    this.clusterUri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ty-db-xadwv.mongodb.net/${databaseName}?retryWrites=true&w=majority`
+
+// connection = await MongoClient.connect(global.__MONGO_URI__, {
+//   useNewUrlParser: true
+// })
+// db = connection.db(global.__MONGO_DB_NAME__)
+
+class Database{
+  constructor(db){
+    this.db = db
+    this.teams = this.db.collection('teams')
   }
 
-  get _client() {
-    this.client = MongoClient(this.clusterUri, {useNewUrlParser: true})
-  }
-
-  saveTeam (teamID, accessTokenCipher) {
-    this._client()
-    this.client.connect(async(err) => {
-      if (err) {
-        console.error(`error connecting to database: ${err}`)
-      } else {
-        var teams = this.client.db('markdownlinksdb').collection('teams')
-        await this._checkTeam(teamID, accessTokenCipher, teams)
-      }
-    })
-    this.client.close()
-  }
-
-  async _checkTeam (teamId, accessTokenCipher, teams) {
-    /* look for an existing team record */
-    var teamRecord = await teams.findOne({
-      team_id: teamId
+  async storeTeamToken (teamID, accessTokenCipher) {
+    
+    var teamRecord = await this.teams.findOne({
+      team_id: teamID
     })
     if (teamRecord) {
-      await teams.findOneAndUpdate({
-        team_id: teamId
+      await this.teams.findOneAndUpdate({
+        team_id: teamID
       },
       {
         $set:
             { access_token_cipher: accessTokenCipher }
       })
     } else if (!teamRecord) {
-      await teams.insertOne({
-        team_id: teamId,
+      await this.teams.insertOne({
+        team_id: teamID,
         access_token_cipher: accessTokenCipher
       })
     }
