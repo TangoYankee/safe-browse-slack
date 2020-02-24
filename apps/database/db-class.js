bad41157c1b7524f7a653127e65c87f53912d900
tangoyankee <miller.tim108@gmail.com>
2020-02-23 22:00:29 -0800

    use test and token crypto for database test data

--- apps/database/db-class.js
+++ apps/database/db-class.js
@@ -1,14 +1,6 @@
 'use strict'
 
-// const MongoClient = require('mongodb').MongoClient
-// const process = require('process')
-
-// var clusterUri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ty-db-xadwv.mongodb.net/markdownlinksdb?retryWrites=true&w=majority`
-
-// connection = await MongoClient.connect(global.__MONGO_URI__, {
-//   useNewUrlParser: true
-// })
-// db = connection.db(global.__MONGO_DB_NAME__)
+const MongoClient = require('mongodb').MongoClient
 
 class Database {
   constructor (dbName) {
