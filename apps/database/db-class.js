f6b35867505c5599c25805ca9313a700956929f6
tangoyankee <miller.tim108@gmail.com>
2020-02-23 16:05:29 -0800

    linting

--- apps/database/db-class.js
+++ apps/database/db-class.js
@@ -1,24 +1,22 @@
 'use strict'
 
-const MongoClient = require('mongodb').MongoClient
+// const MongoClient = require('mongodb').MongoClient
 // const process = require('process')
 
 // var clusterUri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ty-db-xadwv.mongodb.net/markdownlinksdb?retryWrites=true&w=majority`
-
 
 // connection = await MongoClient.connect(global.__MONGO_URI__, {
 //   useNewUrlParser: true
 // })
 // db = connection.db(global.__MONGO_DB_NAME__)
 
-class Database{
-  constructor(db){
+class Database {
+  constructor (db) {
     this.db = db
     this.teams = this.db.collection('teams')
   }
 
   async storeTeamToken (teamID, accessTokenCipher) {
-    
     var teamRecord = await this.teams.findOne({
       team_id: teamID
     })
