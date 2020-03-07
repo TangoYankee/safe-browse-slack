d756db2200fa748fbad2edbbd975ff59336956a5
tangoyankee <miller.tim108@gmail.com>
2020-03-07 15:57:51 -0800

    safebrowse options

--- apps/safe-browse/safe-browse-class.js
+++ apps/safe-browse/safe-browse-class.js
@@ -7,6 +7,16 @@ class SafeBrowse {
     this.threatEntries = threatEntries
   }
 
+  get _options() {
+    return {
+      url: 'https://safebrowsing.googleapis.com/v4/threatMatches:find',
+      body: this._requestBody,
+      json: true,
+      timeout: 1500,
+      qs: { key: process.env.GOOGLE_SAFE_BROWSING_KEY }
+    }
+  }
+
   get _requestBody() {
     return {
       client: {
