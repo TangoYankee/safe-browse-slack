815cf889dd49602a0c369b3ac9910b9324361221
tangoyankee <miller.tim108@gmail.com>
2020-03-18 10:44:05 -0700

    cache threat object

--- apps/threat-cache/__tests__/threat-cache-test.js
+++ apps/threat-cache/__tests__/threat-cache-test.js
@@ -1,9 +1,33 @@
 'use strict'
 
 const ThreatCache = require('../threat-cache')
+const ThreatReportData = require('../test-data/threat-report-data')
 
-describe('manage the threat cache', ()=>{
-  it('should exist', ()=>{
-    expect(new ThreatCache().exists).toBe(true)
+describe('manage the threat cache', () => {
+  var threatCache = new ThreatCache()
+  var threatReportData = new ThreatReportData()
+
+  afterAll(() => {
+    threatCache.terminate()
+  })
+  // Move to Threat Report
+  it('should parse time into an integer', () => {
+    expect(threatCache._duration('300s')).toEqual(300)
+  })
+
+  it('should make url objects cache friendly', () => {
+    expect(threatCache._metaDataStore(threatReportData.forCacheStore)).toEqual(threatReportData.cacheFriendly)
+  })
+
+  it('should save the threat report to the cache', () => {
+    expect(threatCache.store(threatReportData.forCacheStore)).toBe(true)
+  })
+
+  it('should recieve a threat report of urls already in the cache', () => {
+    expect(threatCache.report(threatReportData.userSubmittedUrls)).toEqual(threatReportData.urlsInCache)
+  })
+
+  it('should flush the cache of all data', () => {
+    expect(threatCache.clearData).toEqual(expect.objectContaining({ keys: 0 }))
   })
 })
--- apps/threat-cache/threat-cache.js
+++ apps/threat-cache/threat-cache.js
@@ -1,16 +1,54 @@
 'use strict'
 
+const { cacheStart, cacheInstance } = require('../cache/cache')
+
+cacheStart()
+const cache = cacheInstance()
+
 class ThreatCache {
-  constructor() {
-    this.exists = true
+  report (urls) {
+    return cache.mget(this._metaDataReport(urls))
   }
 
-  get report() {
-    return ''
+  _metaDataReport (urls) {
+    return Object.keys(urls)
   }
 
-  store () {
-    return ''
+  store (forCacheThreatReport) {
+    return cache.mset(this._metaDataStore(forCacheThreatReport))
+  }
+
+  // Move to threat report object
+  _duration (timeLetters) {
+    /* convert time from text to number */
+    var numberRegex = /[0-9]/g
+    var numbers = timeLetters.match(numberRegex)
+    var timeNumbers = numbers.join('')
+    return parseInt(timeNumbers)
+  }
+
+  _metaDataStore (forCacheThreatReport) {
+    /* cache-friendly data format */
+    var storeMetaData = []
+    for (var [urlKey, value] of Object.entries(forCacheThreatReport)) {
+      storeMetaData.push({
+        key: urlKey,
+        val: {
+          threatMatch: value.threatMatch
+        },
+        ttl: value.cacheDuration
+      })
+    }
+    return storeMetaData
+  }
+
+  get clearData () {
+    cache.flushAll()
+    return cache.getStats()
+  }
+
+  get terminate () {
+    cache.close()
   }
 }
 
