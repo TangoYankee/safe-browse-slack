9bf518e68de8cc9187313857b6fa77847cb887a3
tangoyankee <miller.tim108@gmail.com>
2020-03-19 10:46:29 -0700

    split test blocks. add block report tests

--- apps/threat-reports/__tests__/threat-reports-test.js
+++ apps/threat-reports/__tests__/threat-reports-test.js
@@ -1,11 +1,14 @@
 'use strict'
 
 const ThreatReports = require('../threat-reports')
-const ThreatReportData = require('../../__test-data__/threat-report-data')
+const ThreatReportData = require('../test-data/threat-report-data')
 
-var urls = ['testsafebrowsing.appspot.com/s/phishing.html', 'testsafebrowsing.appspot.com/s/unwanted.html', 'testsafebrowsing.appspot.com/s/malware.html', 'nasa.gov']
-
-describe('threats found in cache and safebrowse', () => {
+describe('retrieve urls not stored in cache', () => {
+  var urls = [
+    'testsafebrowsing.appspot.com/s/phishing.html',
+    'testsafebrowsing.appspot.com/s/unwanted.html',
+    'testsafebrowsing.appspot.com/s/malware.html',
+    'nasa.gov']
   var threatReportData = new ThreatReportData(urls)
   var threatReports = new ThreatReports(urls)
 
@@ -15,5 +18,45 @@ describe('threats found in cache and safebrowse', () => {
       'testsafebrowsing.appspot.com/s/unwanted.html',
       'nasa.gov'
     ])
+  })
+})
+
+
+describe('consolidates possible lookup responses', () => {
+  var urls = []
+  var threatReportData = new ThreatReportData(urls)
+  var threatReports = new ThreatReports(urls)
+
+  it('should consolidate error lookup response into empty array', () => {
+    threatReports.lookupAPIReport.messages = true
+    expect(threatReports.toCache).toEqual([])
+  })
+
+  it('should turn empty lookup responses into empty arrays', () => {
+    threatReports.lookupAPIReport = { body: {} }
+    expect(threatReports.toCache).toEqual([])
+  })
+
+  it('should return threat matches reported by lookup API', ()=>{
+    threatReports.lookupAPIReport = threatReportData.lookupAPIReport
+    expect(threatReports.toCache).toEqual(threatReportData.lookupAPIReport.matches)
+  })
+})
+
+describe('consolidates reports to send to block builder', () => {
+  it.skip('should have no threats anywhere', () => {
+
+  })
+
+  it.skip('should have threats in cache and a lookup error', () => {
+
+  })
+
+  it.skip('should have uls in cache, lookup api, and with no threats found', ()=> {
+
+  })
+
+  it.skip('should create deep copies of notInCache and toBlocksReport', () => {
+    
   })
 })
---
+++ apps/threat-reports/test-data/threat-report-data.js
@@ -0,0 +1,68 @@
+'use strict'
+
+class threatReportData {
+  constructor (urls) {
+    this.urls = urls
+    // this.userSubmittedUrls = {
+    //   'testsafebrowsing.appspot.com/s/phishing.html': {},
+    //   'testsafebrowsing.appspot.com/s/unwanted.html': {},
+    //   'testsafebrowsing.appspot.com/s/malware.html': {},
+    //   'nasa.gov': {}
+    // }
+
+    this.urlsInCache = {
+      'testsafebrowsing.appspot.com/s/malware.html': { threatMatch: 'MALWARE' },
+      'testsafebrowsing.appspot.com/s/phishing.html': { threatMatch: 'SOCIAL_ENGINEERING' }
+    }
+
+    // this.forCacheStore = {
+    //   'testsafebrowsing.appspot.com/s/phishing.html': {
+    //     threatMatch: 'SOCIAL_ENGINEERING',
+    //     cacheDuration: 300
+    //   },
+    //   'testsafebrowsing.appspot.com/s/malware.html': {
+    //     threatMatch: 'MALWARE',
+    //     cacheDuration: 300
+    //   }
+    // }
+
+    this.lookupAPIReport = {matches :[
+      {
+        threatType: 'SOCIAL_ENGINEERING',
+        platformType: 'ANY_PLATFORM',
+        threat: {
+          url: 'testsafebrowsing.appspot.com/s/phishing.html'
+        },
+        cacheDuration: '300s',
+        threatEntryType: 'URL'
+      },
+      {
+        threatType: 'MALWARE',
+        platformType: 'ANY_PLATFORM',
+        threat: {
+          url: 'testsafebrowsing.appspot.com/s/malware.html'
+        },
+        cacheDuration: '300s',
+        threatEntryType: 'URL'
+      }
+    ]
+  }
+
+    this.cacheFriendly = [{
+      key: 'testsafebrowsing.appspot.com/s/phishing.html',
+      val: {
+        threatMatch: 'SOCIAL_ENGINEERING'
+      },
+      ttl: 300
+    },
+    {
+      key: 'testsafebrowsing.appspot.com/s/malware.html',
+      val: {
+        threatMatch: 'MALWARE'
+      },
+      ttl: 300
+    }]
+  }
+}
+
+module.exports = threatReportData
