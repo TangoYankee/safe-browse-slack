b3206bb201cde783ae262330b0af396091bc6c74
tangoyankee <miller.tim108@gmail.com>
2020-03-19 14:14:06 -0700

    refactor threat cache test data.  store only in local module

--- apps/threat-cache/test-data/threat-cache-data.js
+++ apps/threat-cache/test-data/threat-cache-data.js
@@ -1,30 +1,18 @@
 'use strict'
 
-class threatReportData {
-  constructor (urls) {
-    this.urls = urls
-    this.userSubmittedUrls = {
-      'testsafebrowsing.appspot.com/s/phishing.html': {},
-      'testsafebrowsing.appspot.com/s/unwanted.html': {},
-      'testsafebrowsing.appspot.com/s/malware.html': {},
-      'nasa.gov': {}
-    }
+class threatCacheData {
+  constructor () {
+    this.urls = [
+      'testsafebrowsing.appspot.com/s/phishing.html',
+      'testsafebrowsing.appspot.com/s/unwanted.html',
+      'testsafebrowsing.appspot.com/s/malware.html',
+      'nasa.gov'
+    ]
 
     this.urlsInCache = {
       'testsafebrowsing.appspot.com/s/malware.html': { threatMatch: 'MALWARE' },
       'testsafebrowsing.appspot.com/s/phishing.html': { threatMatch: 'SOCIAL_ENGINEERING' }
     }
-
-    // this.forCacheStore = {
-    //   'testsafebrowsing.appspot.com/s/phishing.html': {
-    //     threatMatch: 'SOCIAL_ENGINEERING',
-    //     cacheDuration: 300
-    //   },
-    //   'testsafebrowsing.appspot.com/s/malware.html': {
-    //     threatMatch: 'MALWARE',
-    //     cacheDuration: 300
-    //   }
-    // }
 
     this.forCacheStore = [
       {
@@ -64,4 +52,4 @@ class threatReportData {
   }
 }
 
-module.exports = threatReportData
+module.exports = threatCacheData
