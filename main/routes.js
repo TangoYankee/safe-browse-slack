f00946be7ac904462e5d201d6dd27ab7b02f8ebe
tangoyankee <miller.tim108@gmail.com>
2020-03-27 11:03:31 -0700

    add /safebrowse/ to header and footer

--- main/routes.js
+++ main/routes.js
@@ -8,8 +8,7 @@ const {
 } = require('./controllers')
 
 const router = express.Router()
-router.get('/', redirectToLanding)
-router.get('/safebrowse', getLanding)
+router.get('/', getLanding)
 router.get('/privacy', getPrivacy)
 router.get('/oauth', getOAuth)
 router.post('/sb-command', postSafeBrowse)
